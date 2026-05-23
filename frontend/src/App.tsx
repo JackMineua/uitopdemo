4e0cf8e997539f3ab4718faedae8c00b2d192d55
Mikhail <pausedev12@gmail.com>
2026-05-23 14:04:27 +0300

    main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import './App.css';
